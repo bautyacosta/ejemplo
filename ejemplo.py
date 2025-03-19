c51eb0a0b0c3338ae0e118c1b26d6794a705add6
Bauty Acosta <bauty260106@gmail.com>
2025-03-19 11:33:03 -0300

    segundo commit

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,13 @@
+import random 
+import string
+
 print("generador de claves!")
+
+chars = string.ascii_letters + string.digits + string.punctuation
+password = ""
+lenght = 10
+
+for _ in range(lenght):
+    password = password + random.choice(chars)
+
+print ("la contraseña es ", password)
